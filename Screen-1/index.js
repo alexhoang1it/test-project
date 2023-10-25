1a6a65e3d9a8c01ffd90840f6c427942856b27cd
ace <ace1111@gmail.com>
2023-10-25 11:14:13 +0700

    test commit FE 4

--- Screen-1/index.js
+++ Screen-1/index.js
@@ -1,1 +1,26 @@
-string 4
+import React, { useState } from "react";
+  import ReactDom from "react-dom";
+  const App = () => {
+    const [count, setCount] = useState(0);
+  
+    const handleOnClick = () => {
+      setCount(count + 1);
+    };
+  
+    return (
+      <>
+        <p className="text">{count}</p>
+        <form className="background">
+          <label className="text" for="fname">First name:</label><br/>
+          <input type="text" id="fname" name="fname"/><br/>
+          <label className="text" for="lname">Last name:</label><br/>
+          <input type="text" id="lname" name="lname"/>
+        </form>
+        <button onClick={handleOnClick}>Sign up</button>
+      </>
+    );
+  };
+  
+  ReactDom.render(<App />, document.getElementById("app"));
+
+  
